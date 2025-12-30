056cfca18d30ff08c4625018f3fe323d2c01b656
Superiormaster <ejeziepaschal@gmail.com>
2025-12-30 11:16:28 +0100

    Init

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,19 @@ import os
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    
+    import os
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    database_url = os.getenv("DATABASE_URL")
+    if database_url:
+        app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///demo.db'
+
     # detect environment
     env = os.getenv("FLASK_ENV", "development")
 
