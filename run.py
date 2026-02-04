6b2756d0422162025f3710edee5f85539905caab
Superiormaster <ejeziepaschal@gmail.com>
2026-02-04 14:50:34 +0100

    Fix run.py by removing merge conflict markers

--- run.py
+++ run.py
@@ -5,12 +5,4 @@ app = create_app()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-<<<<<<< HEAD
-<<<<<<< HEAD
-    app.run(host="0.0.0.0", port=port)
-=======
     app.run(host="0.0.0.0", port=port, debug=True)
->>>>>>> staging
-=======
-    app.run(host="0.0.0.0", port=port, debug=True)
->>>>>>> staging
