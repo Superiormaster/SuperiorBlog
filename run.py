275962f1e1783c601e174084610bd245864382da
Superiormaster <ejeziepaschal@gmail.com>
2026-02-18 14:57:12 +0100

    New changes5

--- run.py
+++ run.py
@@ -1,14 +1,15 @@
 import os
 from dotenv import load_dotenv
 from apscheduler.schedulers.background import BackgroundScheduler
-from .utils.scheduler import start_scheduler
+from app.utils.scheduler import start_scheduler
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 from app import create_app
 
 app = create_app()
-start_scheduler(app)
+if os.environ.get("RUN_MAIN") == "true" or not os.environ.get("FLASK_RUN_FROM_CLI"):
+    start_scheduler(app)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
