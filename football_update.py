d4f9a2015bb3cfbfae9269bc4acae27eb80eacd3
Superiormaster <ejeziepaschal@gmail.com>
2026-09-05 19:25:13 +0100

    con

--- football_update.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from app.utils.football import update_football_cache
-
-app = create_app()
-with app.app_context():
-    print("Updating football cache...")
-    update_football_cache()
-    print("Done ✅")
